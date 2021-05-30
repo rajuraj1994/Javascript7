505c751084ab7d5215c2dbf3c02b494d11c41e6b
rajuraj1994 <rajurajkandel@gmail.com>
2021-05-30 08:11:14 +0545

    regular expression

--- formvalidation.js
+++ formvalidation.js
@@ -9,6 +9,13 @@ function fnameValidate(){
         displayMsg("firstname must be more than two characters","fnameMsg","red")
         return false;
     }
+    else if(!first_name.match(/^([a-zA-Z])+$/)){  
+        //^->it checks from the start
+        //$->it checks from the last
+        displayMsg("firstname only contains alphabets","fnameMsg","red")
+        return false;
+
+    }
     else{
         displayMsg("valid firstname","fnameMsg","green")
         return true
@@ -27,6 +34,10 @@ let last_name=document.getElementById('lname').value;
         displayMsg("lastname must be more than two characters","lnameMsg","red")
         return false;
     }
+    else if(!last_name.match(/^([a-zA-Z])+$/)){
+        displayMsg("lastname contains only alphabets","lnameMsg","red")
+        return false;
+    }
     else{
         displayMsg("valid lastname","lnameMsg","green")
         return true
@@ -40,6 +51,11 @@ function emailValidate(){
             displayMsg("email is required","emailMsg","red")
             return false;
         }
+        else if(!email.match(/^([a-zA-Z])[a-z0-9\_]+\@+(([a-z])+\.)+([com])+$/)){
+            //ram123@gmail.com
+            displayMsg("invalid email","emailMsg","red")
+            return false;
+        }
        
         else{
             displayMsg("valid email","emailMsg","green")
@@ -48,6 +64,28 @@ function emailValidate(){
     }
     
     
+function passwordValidate(){
+    let pass=document.getElementById('password').value;
+        if(pass==""){
+            //call the function
+            displayMsg("password is required","passMsg","red")
+            return false;
+        }
+       else if(!pass.match(/^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[@$!%*#?&]).{8,40}$/)){
+           //?= matches the required expression  but doesnot depend on their position 
+           //ie matches anywhere in the string position doesnot matter
+           //. matches single character
+           //* matches preceding character 
+           displayMsg("must contain 8 characters,One Uppercase,one lowercase,one digit and one special character"
+           ,"passMsg","red")
+           return false;
+
+       }
+        else{
+            displayMsg("valid password","passMsg","green")
+            return true
+        }
+    }
 
 
 
